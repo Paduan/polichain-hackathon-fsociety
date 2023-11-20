c61141e1d214cce88700f6414f1376e6cc950bfb
pertodomato <guilhermebeccato@gmail.com>
2023-11-20 16:56:31 -0300

    mudei o link do redirect da pagina1

--- front-end/pagina1/script.js
+++ front-end/pagina1/script.js
@@ -2,6 +2,6 @@ function redirecionar() {
     var valorInput = document.getElementById("searchInput").value;
 
     if (valorInput === "fsociety") {
-        window.location.href = "https://fsociety-2.vercel.app/";
+        window.location.href = "https://polichain-hackathon-2.vercel.app/";
     }
 }
